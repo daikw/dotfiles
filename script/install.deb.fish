149e61b7f10de1e7c331757f7a6ff657b54c749a
daikw <daikw@yak-shaver.com>
2022-05-15 20:56:13 +0900

    :+1: add packages

--- script/install.deb.fish
+++ script/install.deb.fish
@@ -6,6 +6,6 @@ set WORKING_DIR (dirname (status -f))
 echo sudo apt install -y (cat $WORKING_DIR/install.deb.packages.txt | tr '\n' ' ')
 
 # others
-echo "curl -sS https://webinstall.dev/zoxide | bash"
-echo "curl -sS https://starship.rs/install.sh | sh"
-echo "curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh"
+# echo "curl -sS https://webinstall.dev/zoxide | bash"
+# echo "curl -sS https://starship.rs/install.sh | sh"
+# echo "curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh"
